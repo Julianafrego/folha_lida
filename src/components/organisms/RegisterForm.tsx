bb1918aaac74303f32b1c8950214b988934e2d3d
julianafrego <julianafgomes@aluno.uespi.br>
2026-07-28 12:17:47 -0300

    feat: atualiza pagina de cadastro com verificação de campos

--- src/components/organisms/RegisterForm.tsx
+++ src/components/organisms/RegisterForm.tsx
@@ -13,12 +13,21 @@ export function RegisterForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+  
   const [formError, setFormError] = useState("");
 
   const handleSubmit: React.ComponentProps<"form">["onSubmit"] = (event) => {
     event.preventDefault();
     setFormError("");
-
+    if (name === "" || email === "" || password === "") {
+      setFormError("Preencha todos os campos corretamente.");
+      return;
+    } else if (criteriosDeSenha(password).valido === false) {
+      return;
+    } else if (validarMesmaSenha(ConfirmPassword, password).valido === false) {
+      return;
+    } 
     try {
       register({ name, email, password });
       router.push("/login");
@@ -28,6 +37,24 @@ export function RegisterForm() {
       );
     }
   };
+
+  function criteriosDeSenha(senha: string) {
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (regex.test(senha)) return { valido: true, texto: "" };
+    else
+      return {
+        valido: false,
+        texto:
+          "A senha deve ter pelo menos 8 caracteres, incluindo letras e números.",
+      };
+  }
+
+  function validarMesmaSenha(confsenha: string, senha: string) {
+    console.log("Valores: " + senha + " " + confsenha);
+    if (senha === confsenha) return { valido: true, texto: "" };
+    else return { valido: false, texto: "Senha não confere!" };
+  }
+  
 
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
@@ -56,7 +83,40 @@ export function RegisterForm() {
         placeholder="Digite sua senha"
         value={password}
         onChange={(event) => setPassword(event.target.value)}
+
+        onBlur={(event) => {
+          setPassword(event.target.value);
+          console.log("Mandou: " + event.target.value);
+          criteriosDeSenha(event.target.value);
+        }}
       />
+
+      <FormField
+        id="confirmPassword"
+        label="Confirmar Senha"
+        type="password"
+        placeholder="Digite sua senha novamente"
+        value={ConfirmPassword}
+        onChange={(event) => setConfirmPassword(event.target.value)}
+
+        onBlur={(event) => {
+          setConfirmPassword(event.target.value);
+          console.log("Mandou: " + [password, event.target.value]);
+          validarMesmaSenha(event.target.value, password);
+        }}
+      />
+
+      {criteriosDeSenha(password).valido === false ? (
+        <p className="text-sm text-red-600">{criteriosDeSenha(password).texto}</p>
+      ) : null
+      }
+
+      {validarMesmaSenha(ConfirmPassword, password).valido === false ? (
+        <p className="text-sm text-red-600">
+          {validarMesmaSenha(ConfirmPassword, password).texto}
+        </p>
+      ) : null
+      }
 
       {formError ? <p className="text-sm text-red-600">{formError}</p> : null}
 
